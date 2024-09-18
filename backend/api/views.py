0095423d0c471de43b296e3c3829b55a6963b8f0
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-18 20:37:06 +0545

    Get Notes On Basis Of User

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
+
+from backend.api.models import Note
 from .serializer import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
 
@@ -10,3 +12,12 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class CreateNoteList(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author = user)
