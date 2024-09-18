8971ec17925eaf5ef353ec5cf4092de28f1176f1
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-18 20:31:08 +0545

    NoteSerializer Created

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from backend.api.models import Note
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -9,4 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        return user
+        return 
+    
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note,
+        fields = ["id","title","content","created_at","author"]
+        extra_kwargs = {"author": {"read_only": True}}
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializer import UserSerializer
+from .serializer import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
 
 
