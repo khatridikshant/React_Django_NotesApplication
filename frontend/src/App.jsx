42965819b9f398871de0c29f2b280f62523db991
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-22 20:22:44 +0545

    Routing Logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route, Navigation, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import { Register } from './pages/Register'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
+
+function Logout() { 
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    
-      <div>
-         </div>
-        
+      
+
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={
+        <ProtectedRoute><Home />
+      </ProtectedRoute>
+    } />
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path = '/register' element={<RegisterAndLogout/>} />
+      <Route path='*' element={<NotFound/>}></Route>
+    </Routes>
+        </BrowserRouter>
     
   )
 }
