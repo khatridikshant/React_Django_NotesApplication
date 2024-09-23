0c7c4b71c34975f109359d654772646d72acca5e
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-23 18:59:56 +0545

    Create Notes Form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,14 +35,63 @@ export const Home = () => {
   };
 
   const createNote = (e) => {
-    e.preventDefault()
-    api.post("/api/notes/", {content,title}).then((response) => {
-      if (response.status === 201){
-        alert("Note Created")
-      } else {alert("Failed To Note Created")}
-    }
-  )
-  }
+    e.preventDefault();
+    api.post("/api/notes/", { content, title }).then((response) => {
+      if (response.status === 201) {
+        alert("Note Created");
+      } else {
+        alert("Failed To Note Created");
+      }
+    });
+  };
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col">
+            <h1>Notes</h1>
+          </div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <h2>Create A Note</h2>
+        </div>
+
+        <div className="row">
+          <form action="" onSubmit={createNote}>
+            <div class="mb-3">
+              <label for="title" class="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                name="title"
+                onChange={(e) => setTitle(e.target.value) }
+                value = {title}
+                
+              />
+            </div>
+            <div class="mb-3">
+              <label for="content" class="form-label">
+                Content
+              </label>
+              <textarea
+                class="form-control"
+                id="content"
+                rows="3"
+              ></textarea>
+            </div>
+            <div className="mb-3"><input type="submit" value="Submit" /></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
